7672a84f92618d95236540b960b75f6119036ee9
bobbyrashford <kemalcleva@gmail.com>
2024-02-16 21:57:34 +0100

    desc

--- components/Rules.js
+++ components/Rules.js
@@ -16,13 +16,15 @@ const Rules = () => {
         How To Play
       </div>
       <div className={style.rule}>
-        Get an entry for 0.1 BNB, you can enter multiple times. One winner per game and he gets 1.9 BNB, because 5% is burned each game!
+        Get an entry for XXXX $HUSKY, the prize is almost 20x times the entry amount. There are maximum 20 entries and the winner gets 95% of the entries. 5% will be burned automatically each game!
       </div>
       <div className={style.rule}>
-        The daily winner will be chosen randomly by the smart contract. Since each entry is added to a fixed 20-spots array, each entry from the same person - even with different addresses - statistically increases the chances to win. 
+        To play the game on mobile, you will need to open the website from the internal browser of MetaMask, then you will be able to connect the wallet. Remember to be on BSC chain before connecting!
       </div>
       <div className={style.rule}>
-        To play the game on mobile, you need to open the website from the internal browser of MetaMask, then you will be able to connect the wallet. Remember to be on BSC chain before connecting!
+        This is a very brief technical explanation of the lottery: <i>the daily winner will be chosen randomly by the smart contract by a random index number mod 20, keep in mind that Solidity arrays save each entry differently, this means that the contract won&apos;t 
+        save your address as a lottery participant ID, the actual ID will be the position (index) when you entered the lottery, therefore suppose you bet 10 times of 20 available entries, you will have statistically 50% chances to win the final prize. If the array was going to save your address as entry ID,  
+        if you bet 19 times with the same address and another guy bets 1 time only, you would statistically have 50% chances to win, not 95%.</i>
       </div>
       <div className={style.rule}>
         For reference, you can chek the public contract code at this address: <button onClick={copyToClipboard}>0x8bc237fdade26044af12ab9d2685994048a639c3</button>
