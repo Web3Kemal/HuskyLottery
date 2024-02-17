f9f162c87ace92ac3ce77af45f0ac7caf82ea1a8
bobbyrashford <kemalcleva@gmail.com>
2024-02-17 19:18:38 +0100

    HOPEFULLY LAST GRAPHIC UPDT

--- components/Rules.js
+++ components/Rules.js
@@ -14,13 +14,13 @@ const Rules = () => {
   return (
     <div className={style.wrapper}>
       <div className={style.title}>
-        How To Play
+        HOW TO PLAY
+      </div>
+      <div className={style.rule}>
+        <b>If you&apos;re on mobile, please use your wallet browser (e.g. MetaMask) and make sure to be on Binance Smart Chain network to play. You can easily add the network in MetaMask.</b>
       </div>
       <div className={style.rule}>
         Get an entry for XXXX $HUSKY, the prize is almost 20x times the entry amount. There are maximum 20 entries and the winner gets 95% of the entries. 5% will be burned automatically each game!
-      </div>
-      <div className={style.rule}>
-        To play the game on mobile, you will need to open the website from the internal browser of MetaMask, then you will be able to connect the wallet. Remember to be on BSC chain before connecting!
       </div>
       <div className={style.rule}>
         This is a very brief technical explanation of the lottery: <i>the daily winner will be chosen randomly by the smart contract by a random index number mod 20, keep in mind that Solidity arrays save each entry differently, this means that the contract won&apos;t 
