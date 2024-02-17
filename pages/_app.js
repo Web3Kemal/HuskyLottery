1087178da06929ed3b702dd94d265ed026d2607f
bobbyrashford <kemalcleva@gmail.com>
2024-02-17 19:08:47 +0100

    update context & added head to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.css'
 import { AppProvider } from '../context/context'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AppProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        {/* Other meta tags and stylesheets go here */}
+      </Head>
       <Component {...pageProps} />
     </AppProvider>
   )
